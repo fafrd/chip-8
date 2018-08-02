9acb584b3bcb91643b8dad3a2b69d5d934abf315
Kian <kian@horsefucker.org>
2018-08-01 20:22:42 -0400

    pointer wrangling

--- disassembler.c
+++ disassembler.c
@@ -1,24 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
+void readFile(char *filename, char **buffer, int *length)
 {
-	char *filename = "pong.rom";
 	FILE *f = fopen(filename, "r");
 
 	fseek(f, 0, SEEK_END);
-	int length = ftell(f);
+	*length = ftell(f);
 	fseek(f, 0, SEEK_SET);
 
-	char *buffer = malloc(length);
-	fread(buffer, length, 1, f);
+	*buffer = malloc(*length);
+	fread(*buffer, *length, 1, f);
 	fclose(f);
+}
 
+void printBytes(char *buffer, int length)
+{
 	for (int i = 0; i < length; i++)
 	{
 		printf("%hhx ", buffer[i]);
 	}
 
 	printf("%c", '\n');
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc < 2)
+	{
+		printf("%s", "usage: disassemble file.rom\n");
+		return -1;
+	}
+
+	char *buffer;
+	int length;
+	readFile(argv[1], &buffer, &length);
+
+	printBytes(buffer, length);
+
 	free(buffer); // it's the right thing to do
 }
