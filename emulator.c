552980029e85abdfae378a91c57facf440be7258
kian <kbradley@ucmerced.edu>
2018-08-02 22:00:22 -0400

    start on main program loop

--- emulator.c
+++ emulator.c
@@ -15,9 +15,69 @@ int loadRomToMemory(char **buffer, size_t length)
 }
 
 // main instruction processing loop
-void loop(void *location)
+void loop()
 {
+	// program starts at 0x200
+	r_pc = 0x200;
+	char current_upper, current_lower;
+	short current;
 
+	while (1)
+	{
+		// set current instruction...
+		// instructions are 2 bytes, get upper+lower byte, combine
+		current_upper = mem[r_pc];
+		current_lower = mem[r_pc+1];
+		current = (((short)current_upper) << 8) | current_lower;
+
+		// switch on first nibble (4 bits)
+		switch (current_upper >> 4)
+		{
+			case 0x0:
+			{
+				if (current == 0x00e0)
+					// TODO CLS - clear screen
+				else if (current == 0x00ee)
+					// TODO RET - return from subroutine
+			}
+			case 0x1:
+			{
+				short jumpDest = current & 0xfff;
+				// TODO JP - jump
+			}
+			case 0x2:
+
+			case 0x3:
+
+			case 0x4:
+
+			case 0x5:
+
+			case 0x6:
+
+			case 0x7:
+
+			case 0x8:
+
+			case 0x9:
+
+			case 0xa:
+
+			case 0xb:
+
+			case 0xc:
+
+			case 0xd:
+
+			case 0xe:
+
+			case 0xf:
+
+		}
+
+		// increment pc by 2 bytes
+		r_pc += 2;
+	}
 }
 
 int main(int argc, char *argv[])
@@ -42,6 +102,9 @@ int main(int argc, char *argv[])
 		printf("%s", "error: ROM size greater than 0xc8f bytes\n");
 		return 1;
 	}
+
+	// start main program processing loop
+	loop();
 
 	//printBytes(buffer, length);
 
